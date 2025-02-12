7f7aa7df82a696aef1c28a21fc1792ee71adc449
Brais16 <bmanent19763@iesjoanramis.org>
2025-02-12 13:20:39 +0100

    Ex7

---
+++ Tasca11.Ex7.py
@@ -0,0 +1,3 @@
+paraula = input("Digues la paraula i la separaré: ")
+llista_de_lletres = [lletra for lletra in paraula]
+print(llista_de_lletres)  # Ha de retornar ['i', 'n', 's', 't', 'i', 't', 'u', 't']
